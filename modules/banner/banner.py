cffaeff0f5bdf722cf359ca97ecc6cc2c2e6f319
pristavkaegor03@gmail.com <pristavka_egor>
2025-01-18 23:14:24 +0800

    Banenr encoding fixed

--- modules/banner/banner.py
+++ modules/banner/banner.py
@@ -5,7 +5,7 @@ class BannerWriter:
 
     @staticmethod
     def write_banner():
-        with open(f'{Path(__file__).parent.resolve()}/banner.txt') as f:
+        with open(f'{Path(__file__).parent.resolve()}/banner.txt', 'r', encoding="utf_8_sig") as f:
             lines = f.readlines()
             for i in lines:
                 print(i.replace('\n', ''))
